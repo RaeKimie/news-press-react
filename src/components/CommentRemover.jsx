2433913164c3fb3ea57d930f9066005773ee02a7
RaeKimie <nr.kim91@gmail.com>
2019-12-17 19:03:59 +0000

    added loader to commentremover

--- src/components/CommentRemover.jsx
+++ src/components/CommentRemover.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
 import * as api from "../utils/api";
+import Loader from "./Loader";
 
 class CommentRemover extends Component {
-  state = { isDelete: false };
+  state = { isDelete: false, isLoading: false };
   handleClick = event => {
     this.setState(currentState => {
       return { isDelete: !currentState.isDelete };
     });
   };
   handleDelete = event => {
+    this.setState(currentState => {
+      return { ...currentState, isLoading: true };
+    });
     api.deleteComment(this.props.comment_id).then(() => {
       this.props.removeComment();
     });
   };
   render() {
+    if (this.state.isLoading) return <Loader />;
     return (
       <div>
         {!this.state.isDelete ? (
